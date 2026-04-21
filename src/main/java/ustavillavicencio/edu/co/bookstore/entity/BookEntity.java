71bf4d2e358cb2cab611f38d37cfb9e2d0661f8b
diego <diegobarbosa090305@gmail.com>
2026-04-21 09:19:52 -0500

    Add: AuthorEntity, BookEntity, BookState enum. Delete: Book class.

---
+++ src/main/java/ustavillavicencio/edu/co/bookstore/entity/BookEntity.java
@@ -0,0 +1,48 @@
+package ustavillavicencio.edu.co.bookstore.entity;
+
+import org.springframework.format.annotation.NumberFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import ustavillavicencio.edu.co.enums.BookState;
+
+@Entity
+@Table(name = "books")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class BookEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private BookState state;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(nullable = false)
+    @NumberFormat(pattern = "###-#-##-######-#")
+    private String isbn;
+
+    @ManyToOne(optional = false)
+    private AuthorEntity author;
+}
