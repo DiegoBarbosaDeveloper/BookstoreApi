d9aacd4fc18d4dd1ce5af0aff7614a31f85c62ca
Diego Barbosa <diegobarbosa090305@gmail.com>
2026-04-22 15:34:04 -0500

    Delete src/main/java/ustavillavicencio/edu/co/bookstore/exception directory

--- src/main/java/ustavillavicencio/edu/co/bookstore/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package ustavillavicencio.edu.co.bookstore.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String resourceName, Long id) {
-        super(resourceName + " con id " + id + " no encontrado");
-    }
-}
