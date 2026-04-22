35bd05b9bd146f6b070d8eac39bbfef997f77745
Diego Barbosa <diegobarbosa090305@gmail.com>
2026-04-22 15:48:21 -0500

    Delete src/main/java/ustavillavicencio/edu/co/bookstore/exception/custom/InsufficientStockException.java

--- src/main/java/ustavillavicencio/edu/co/bookstore/exception/custom/InsufficientStockException.java
+++
@@ -1,8 +0,0 @@
-package ustavillavicencio.edu.co.bookstore.exception.custom;
-
-public class InsufficientStockException extends RuntimeException {
-
-    public InsufficientStockException(String title, int available, int requested) {
-        super("Stock insuficiente para '" + title + "': " + "disponible=" + available + ", solicitado=" + requested);
-    }
-}
