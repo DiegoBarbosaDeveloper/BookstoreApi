33fdfb65ccf111c10e24b35485bbdccc13dbddfd
DiegoBarbosaDeveloper <diegobarbosa090305@gmail.com>
2026-04-23 08:25:34 -0500

    add: src/main/java/ustavillavicencio/edu/co/bookstore/controller/BookController.java modify: src/main/java/ustavillavicencio/edu/co/bookstore/dto/response/ApiResponse.java

---
+++ src/main/java/ustavillavicencio/edu/co/bookstore/dto/response/ApiResponse.java
@@ -0,0 +1,27 @@
+package ustavillavicencio.edu.co.bookstore.dto.response;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ApiResponse<T> {
+    private boolean success;
+    private String message;
+    private T data;
+    private List<String> errors;
+    private int errorCode;
+    private long timestamp;
+    private String path;
+
+    
+
+}
